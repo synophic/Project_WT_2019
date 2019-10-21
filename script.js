2ae9cea901b5253a5baae3455cc60964c1c21ffd
stupidwolfy <61070268@kmitl.ac.th>
2019-10-21 21:45:02 +0700

    add carabao
    Sawat dee carp pee nong!!

---
+++ script.js
@@ -0,0 +1,57 @@
+function changePage(pagee) {
+    let requestURL = 'info/info.json';
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            var myJSON = JSON.parse(request.responseText);
+            myFunction(myJSON, pagee);
+        }
+    };
+    request.open("GET", requestURL, true);
+    request.send();
+}
+function myFunction(myObj, page) {
+    document.getElementById
+
+    var table = document.createElement("table");
+    var row = document.createElement("tr");
+    var column = document.createElement("td");
+    table.classList.add("table");
+    table.classList.add("text-center");
+    //document.write(myObj.page1.topic + '<br><br>');
+    for (let i = 0; i < myObj[page].info.length; i++) {
+        //document.write(myObj.page1.info[i].title + '<br>');
+        var row = document.createElement("tr");
+        var hcolumn = document.createElement("th");
+        var column = document.createElement("td");
+        column.innerText = myObj[page].info[i].title;
+        row.appendChild(column);
+        table.appendChild(row);
+        if (myObj[page].info[i].type == "multiple") {
+            var row = document.createElement("tr");
+            var column = document.createElement("td");
+            for (let j = 0; j < myObj[page].info[i].data.length; j++) {
+                //document.write(myObj.page1.info[i].data[j] + '<br><br>');
+                column.innerText += myObj[page].info[i].data[j];
+            }
+            row.appendChild(column);
+            table.appendChild(row);
+        } else {
+            //document.write(myObj.page1.info[i].data + '<br><br>');
+            var row = document.createElement("tr");
+            var column = document.createElement("td");
+            column.innerText = myObj[page].info[i].data;
+            row.appendChild(column);
+            table.appendChild(row);
+        }
+    }
+    document.body.appendChild(table);
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
