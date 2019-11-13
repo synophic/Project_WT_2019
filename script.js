6ac266f2c6c10336128e80f1dc50a6e80fc4fcbf
stupidwolfy <61070268@kmitl.ac.th>
2019-11-13 16:34:52 +0700

    add video run

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function changePage(page) {
             column.innerHTML += '<div class="video-warpper">' +
                 '<div class="video-foreground embed-responsive-16by9">' +
                 '<iframe src="https://www.youtube.com/embed/' +
-                (typeof(myJSON[page].info[i].video != "string") ? myJSON['page1'].info[i].video : myJSON[page].info[i].video) +
+                myJSON[page].info[i].video+
                 '?controls=0&autoplay=1&mute=1&autohide=1&rel=0&loop=1&playlist=0blgmlUY1Qc" frameborder="0" allowfullscreen></iframe>' +
                 '</div>' +
                 '</div>';
