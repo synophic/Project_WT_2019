5477427a267b638fbc9c710af3e49258dbad01cf
stupidwolfy <61070268@kmitl.ac.th>
2019-11-13 14:01:40 +0700

    add cache json
    Now json load only first time.
    Server can go to sleep. ;)

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-function changePage(pagee) {
-    let requestURL = 'info/info.json';
-    let request = new XMLHttpRequest();
+let requestURL = 'info/info.json';
+let request = new XMLHttpRequest();
+var myJSON;
+function load(pagee) {
     request.onreadystatechange = function () {
         if (request.readyState == 4 && request.status == 200) {
-            var myJSON = JSON.parse(request.responseText);
-            myFunction(myJSON, pagee);
+            myJSON = JSON.parse(request.responseText);
+            changePage(pagee);
         }
     };
     request.open("GET", requestURL, true);
     request.send();
 }
-function myFunction(myObj, page) {
+function changePage(page) {
     var elements = document.getElementsByTagName('table');
     while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
 
@@ -19,45 +20,45 @@ function myFunction(myObj, page) {
     var column = document.createElement("td");
     table.classList.add("table");
     table.classList.add("text-center");
-    //document.write(myObj.page1.topic + '<br><br>');
-    for (let i = 0; i < myObj[page].info.length; i++) {
-        //document.write(myObj.page1.info[ i].title + '<br>');
+    //document.write(myJSON.page1.topic + '<br><br>');
+    for (let i = 0; i < myJSON[page].info.length; i++) {
+        //document.write(myJSON.page1.info[ i].title + '<br>');
         var row = document.createElement("tr");
         if (i == 0) {
             var column = document.createElement("th");
             column.innerHTML += '<div class="video-warpper">' +
                 '<div class="video-foreground embed-responsive-16by9">' +
-                '<iframe src="https://www.youtube.com/embed/0blgmlUY1Qc?controls=0&start=3&end=4&autoplay=1&mute=1&showinfo=0&autohide=1&rel=0&loop=1&playlist=0blgmlUY1Qc" frameborder="0" allowfullscreen></iframe>' +
+                '<iframe src="https://www.youtube.com/embed/0blgmlUY1Qc?controls=0&autoplay=1&mute=1&autohide=1&rel=0&loop=1&playlist=0blgmlUY1Qc" frameborder="0" allowfullscreen></iframe>' +
                 '</div>' +
                 '</div>';
-            column.innerHTML += '<div class="vid-info">' + myObj[page].info[i].title + '</div>';
+            column.innerHTML += '<div class="vid-info">' + myJSON[page].info[i].title + '</div>';
         }
         else {
             var column = document.createElement("td");
-            column.innerText = myObj[page].info[i].title;
+            column.innerText = myJSON[page].info[i].title;
         }
         row.appendChild(column);
         table.appendChild(row);
-        if (myObj[page].info[i].type == "multiple") {
+        if (myJSON[page].info[i].type == "multiple") {
             var mini_table = document.createElement("table");
             mini_table.classList.add("table-borderless");
 
-            for (let j = 0; j < myObj[page].info[i].data.length; j++) {
+            for (let j = 0; j < myJSON[page].info[i].data.length; j++) {
                 var mini_row = document.createElement("tr");
                 var row = document.createElement("tr");
                 var column = document.createElement("td");
-                //document.write(myObj.page1.info[i].data[j] + '<br><br>');
-                column.innerText = myObj[page].info[i].data[j];
+                //document.write(myJSON.page1.info[i].data[j] + '<br><br>');
+                column.innerText = myJSON[page].info[i].data[j];
                 mini_row.appendChild(column);
                 mini_table.appendChild(mini_row);
             }
             row.appendChild(mini_table);
             table.appendChild(row);
         } else {
-            //document.write(myObj.page1.info[i].data + '<br><br>');
+            //document.write(myJSON.page1.info[i].data + '<br><br>');
             var row = document.createElement("tr");
             var column = document.createElement("td");
-            column.innerText = myObj[page].info[i].data;
+            column.innerText = myJSON[page].info[i].data;
             row.appendChild(column);
             table.appendChild(row);
         }
